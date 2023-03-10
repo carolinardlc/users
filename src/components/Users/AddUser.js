694737429949af6e9082318b953792b2681b0024
Carolina R. De La Cuba <carolina.rdc@outlook.com>
2023-03-10 18:46:39 -0500

    Some stylingW

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,23 @@
 import React from "react";
 
+import Card from "../UI/Card.js";
+import classes from "./AddUser.module.css";
+
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlForm="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlForm="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">AddUser</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlForm="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlForm="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">AddUser</button>
+      </form>
+    </Card>
   );
 };
 
