e7f313e5632f2597ba590997f31907c267641a59
Carolina R. De La Cuba <carolina.rdc@outlook.com>
2023-03-16 16:39:28 -0500

    UsersList update

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -16,7 +16,7 @@ const AddUser = (props) => {
     if (+enteredAge < 1) {
       return;
     }
-    console.log(enteredUsername, enteredAge);
+    props.onAddUser(enteredUsername, enteredAge);
     setEnteredUsername("");
     setEnteredAge("");
   };
