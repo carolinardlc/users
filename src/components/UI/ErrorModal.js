cf48b9cbdd2ea42c4f3e5727e9048fb0846351b1
Carolina R. De La Cuba <carolina.rdc@outlook.com>
2023-03-18 22:21:59 -0500

    ErrorModal.js update

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -6,17 +6,20 @@ import classes from "./ErrorModal.module.css";
 
 const ErrorModal = (props) => {
   return (
-    <Card className={classes.modal}>
-      <header className={classes.header}>
-        <h2>{props.title}</h2>
-      </header>
-      <div className={classes.content}>
-        <p>{props.message}</p>
-      </div>
-      <footer className={classes.actions}>
-        <button>Okay</button>
-      </footer>
-    </Card>
+    <div>
+      <div className={classes.backdrop} />
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <button>Okay</button>
+        </footer>
+      </Card>
+    </div>
   );
 };
 
