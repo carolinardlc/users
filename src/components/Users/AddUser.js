4913af59974e7c8e790083758dabefd53d295a73
Carolina R. De La Cuba <carolina.rdc@outlook.com>
2023-03-22 00:56:58 -0500

    Re assigned a couple a values

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -31,6 +31,8 @@ const AddUser = (props) => {
       return;
     }
     props.onAddUser(enteredName, enteredUserAge);
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   const errorHandler = () => {
